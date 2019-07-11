3d9b7e401d0accac42f8c1191113f45f7efb65f4
Nikola <haron.dev@gmail.com>
2019-07-11 23:23:33 +0200

    Design added & login activity added

--- app/src/main/java/pl/holker/oraganizeu_android/functionalities/login/LoginActivity.kt
+++ app/src/main/java/pl/holker/oraganizeu_android/functionalities/login/LoginActivity.kt
@@ -1,13 +1,13 @@
-package pl.holker.oraganizeu_android.functionalities
+package pl.holker.oraganizeu_android.functionalities.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import pl.holker.oraganizeu_android.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
---
+++ app/src/main/java/pl/holker/oraganizeu_android/functionalities/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package pl.holker.oraganizeu_android.functionalities.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import pl.holker.oraganizeu_android.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
