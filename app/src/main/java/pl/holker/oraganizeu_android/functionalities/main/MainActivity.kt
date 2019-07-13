6545f34ed84cd17ded690ff597a0e2de004f5e80
Nikola <haron.dev@gmail.com>
2019-07-13 22:18:25 +0200

    Change style of main screen

--- app/src/main/java/pl/holker/oraganizeu_android/functionalities/main/MainActivity.kt
+++ app/src/main/java/pl/holker/oraganizeu_android/functionalities/main/MainActivity.kt
@@ -1,7 +1,9 @@
 package pl.holker.oraganizeu_android.functionalities.main
 
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 import pl.holker.oraganizeu_android.R
 
 class MainActivity : AppCompatActivity() {
