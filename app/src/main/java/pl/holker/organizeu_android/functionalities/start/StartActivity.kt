aa6d29e1244a12e8ff0325219bdd4c2e5aba380d
HolkerDev <haron.dev@gmail.com>
2019-07-18 20:29:49 +0200

    New fragments added

--- app/src/main/java/pl/holker/organizeu_android/functionalities/start/StartActivity.kt
+++ app/src/main/java/pl/holker/organizeu_android/functionalities/start/StartActivity.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
+import kotlinx.android.synthetic.main.activity_start.*
 import pl.holker.organizeu_android.R
 import pl.holker.organizeu_android.databinding.ActivityStartBinding
 import pl.holker.organizeu_android.di.Injectable
 import pl.holker.organizeu_android.di.ViewModelInjectionFactory
+import pl.holker.organizeu_android.functionalities.location_notes.LocationNotesFragment
+import pl.holker.organizeu_android.functionalities.typical_notes.TypicalNotesFragment
 import javax.inject.Inject
 
 class StartActivity : AppCompatActivity(), Injectable {
@@ -24,6 +27,22 @@ class StartActivity : AppCompatActivity(), Injectable {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initBinding()
+        initFragments()
+    }
+
+    private fun initFragments() {
+        mSelectionsPagerAdapter = SelectionsFragmentAdapter(supportFragmentManager)
+        setupViewPager()
+        start_tl_tabs.setupWithViewPager(start_vp_container)
+        start_vp_container.currentItem = 0
+    }
+
+
+    private fun setupViewPager() {
+        val fragmentAdapter = mSelectionsPagerAdapter
+        fragmentAdapter.addFragment("Notes", TypicalNotesFragment())
+        fragmentAdapter.addFragment("Locations", LocationNotesFragment())
+        start_vp_container.adapter = mSelectionsPagerAdapter
     }
 
     private fun initBinding() {
