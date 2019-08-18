301838034a9e575dfa16ddd749c59560a6d13217
Holker <haron.dev@gmail.com>
2019-08-18 12:36:29 +0200

    Add google maps API

--- app/src/main/java/pl/holker/organizeu_android/functionalities/location_notes/LocationNotesFragment.kt
+++ app/src/main/java/pl/holker/organizeu_android/functionalities/location_notes/LocationNotesFragment.kt
@@ -5,12 +5,39 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.fragment_location_notes.*
 import pl.holker.organizeu_android.R
 
-class LocationNotesFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater,
-                              container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+class LocationNotesFragment : Fragment(), OnMapReadyCallback {
+    private lateinit var mMap: GoogleMap
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         return inflater.inflate(R.layout.fragment_location_notes, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment =
+            fragment_location_notes_map as SupportMapFragment?
+        mapFragment?.getMapAsync(this)
     }
 }
---
+++ app/src/main/java/pl/holker/organizeu_android/functionalities/location_notes/LocationNotesVM.kt
@@ -0,0 +1,6 @@
+package pl.holker.organizeu_android.functionalities.location_notes
+
+import androidx.lifecycle.ViewModel
+
+class LocationNotesVM : ViewModel() {
+}
